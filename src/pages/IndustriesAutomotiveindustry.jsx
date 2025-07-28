3f7ed0d40aba0cc6b99ac5b15db06bda79831d70
Kamal <gkamalkumar5@gmail.com>
2025-07-28 12:37:34 +0530

    Kamal : init changes

--- src/pages/IndustriesAutomotiveindustry.jsx
+++ src/pages/IndustriesAutomotiveindustry.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
 import IndustriesAutomotiveBanner from "../components/IndustriesAutomotiveBanner";
-import IndustriesAutomotiveBenifits from "../components/IndustriesAutomotiveBenifits"
+import IndustriesAutomotiveBenifits from "../components/IndustriesAutomotiveBenifits";
 import NincheTechnologiesTestimonialSection from "../components/Ninchetechnologiestestimonials";
-import Ninachetechnologiesbottomsection from "../components/Ninachetechnologiesbottomsection"
+import Ninachetechnologiesbottomsection from "../components/Ninachetechnologiesbottomsection";
 const IndustriesAutomotiveindustry = () => {
   return (
-    <main className ="niche-tech">
-    <div className="body_wrap">
-    <div class="xb-backtotop style-2">
-         <a href="#" class="scroll">
-         <i class="far fa-arrow-up"></i>
-         </a>
+    <main className="niche-tech">
+      <div className="body_wrap">
+        <div class="xb-backtotop style-2">
+          <a href="#" class="scroll">
+            <i class="far fa-arrow-up"></i>
+          </a>
+        </div>
+        <IndustriesAutomotiveBanner></IndustriesAutomotiveBanner>
+        <IndustriesAutomotiveBenifits></IndustriesAutomotiveBenifits>
+        <NincheTechnologiesTestimonialSection></NincheTechnologiesTestimonialSection>
+        <Ninachetechnologiesbottomsection></Ninachetechnologiesbottomsection>
+        );
       </div>
- <IndustriesAutomotiveBanner></IndustriesAutomotiveBanner>
- <IndustriesAutomotiveBenifits></IndustriesAutomotiveBenifits>
- <NincheTechnologiesTestimonialSection></NincheTechnologiesTestimonialSection>
-  <Ninachetechnologiesbottomsection></Ninachetechnologiesbottomsection>
-      );
-    </div>
-  </main>
+    </main>
   );
 };
 
